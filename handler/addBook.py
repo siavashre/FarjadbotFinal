78b517817f723003d7f53a90c54fa02801526cda
Siavash <siavash.raisi@gmail.com>
2018-02-03 19:37:58 +0330

    refactoed

--- handler/addBook.py
+++ handler/addBook.py
@@ -1,10 +1,24 @@
 import json
 from util.keyboards import keyboards
 class AddBook():
+    name=""
+    author=""
+    price=0
+    period=""
+    page_num=0
+    genre=""
+    reader_age=""
+    jeld_num=0
+    summery=""
+    description=""
 
     def __init__(self):
         with open('configs/states.json', 'r') as f:
             self.states = json.load(f)
 
     def addBook(self, bot, message, current_state):
-        print("you are in add boook")
+        if message.text=="عنوان":
+            bot.send_message(chat_id=message.chat_id, text="نام کتاب را وارد کنید")
+            self.name=message.text
+            print(self.name)
+            # return self.states[current_state]["nextState"]["watchMore"]
